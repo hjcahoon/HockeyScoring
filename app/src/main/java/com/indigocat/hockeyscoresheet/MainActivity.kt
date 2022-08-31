53859954dc62c9e858369ac8e201fcaf02a1e95e
HCahoon <hcahoon@envysion.com>
2022-08-31 09:19:19 -0600

    add team logo

--- app/src/main/java/com/indigocat/hockeyscoresheet/MainActivity.kt
+++ app/src/main/java/com/indigocat/hockeyscoresheet/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -18,11 +15,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.indigocat.hockeyscoresheet.ui.theme.*
 
 class MainActivity : ComponentActivity() {
@@ -46,10 +47,12 @@ fun App() {
             Row(modifier = Modifier.padding(12.dp, 16.dp)) {
                 TeamInfo(
                     teamName = "Resurrection Christian",
+                    iconUrl = "https://imagedelivery.net/ErrQpIaCOWR-Tz51PhN1zA/2c6825fd-aa97-404d-34d2-a01236d06000/128",
                     modifier = Modifier.weight(1f),
                 )
                 TeamInfo(
                     teamName = "Monarch Coyotes Varsity",
+                    iconUrl = "https://imagedelivery.net/ErrQpIaCOWR-Tz51PhN1zA/128f7a1e-f8f1-45f3-a2ad-8d2f04230700/128",
                     modifier = Modifier.weight(1f)
                 )
             }
@@ -77,15 +80,33 @@ fun App() {
 }
 
 @Composable
-fun TeamInfo(teamName: String, modifier: Modifier) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+fun TeamInfo(
+    teamName: String,
+    iconUrl: String,
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier
     ) {
-        Score(currentScore = 3)
-        TeamName(text = teamName)
-        ShotButton()
+        AsyncImage(
+            modifier = Modifier.matchParentSize(),
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(iconUrl)
+                .build(),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            alpha = 0.2f
+        )
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Score(currentScore = 3)
+            TeamName(text = teamName)
+            ShotButton()
+        }
     }
+
 }
 
 @Composable
